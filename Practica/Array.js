762ed03499ad26542fc9330ec3a7be4ca1104a18
AlexRomanMamani <amamani@chilesystems.com>
2022-05-05 22:13:16 -0400

    Se agregan comentarios

--- Practica/Array.js
+++ Practica/Array.js
@@ -1,18 +1,23 @@
+//Creacion de arreglo (array)
 const frutas = ["Manzana", "Platano"];
+//Agregar elementos al array
 frutas[2] = "Sandia";
 frutas[3] = "Pera";
 frutas[4] = "Ciruela";
+//Imprimir la cantidad de campos del arreglo (length)
 console.log(frutas.length);
-
+//Asigna valor del numero de indice de un arreglo a una variable.
 let primero = frutas[0];
 let segundo = frutas[frutas.length - 1];
+//Imprimir variables en consola.
 console.log(primero + segundo);
 
 frutas.forEach(function (elemento, indice, array) {
   console.log(elemento, indice);
 });
-
+//Creacion de arreglo (array)
 const cars = ["Saab", "Volvo", "BMW"];
+//Asignacion de elemento al array.
 cars[3] = "Mazda";
 
 console.log(cars.length);
