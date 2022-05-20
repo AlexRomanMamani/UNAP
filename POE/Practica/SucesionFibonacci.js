1fbb3aab70317f6d771687956f60633eca235bb3
AlexRomanMamani <amamani@chilesystems.com>
2022-05-20 01:00:07 -0400

    asd

--- POE/Practica/SucesionFibonacci.js
+++ POE/Practica/SucesionFibonacci.js
@@ -1,12 +1,13 @@
 //Declaracion de variables.
-      var a = 0;
-      var b = 1;
-      var c = 0;
-      var n = 15;
-      //Estructura de control
-      for (let k = 0; k <= n; k++) {
-          c = b+a;
-          a = b;
-          b = c;            
-      }
-      console.log(a);
+var a = 0;
+var b = 1;
+var c;
+var n = 10;
+//Estructura de control
+for (let k = 0; k <= n; k++) {
+  c = b + a;
+  a = b;
+  b = c;
+  console.log(a);
+}
+//console.log(a);
