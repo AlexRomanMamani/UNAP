380d0e16139a7bee592058bffa53e5ff07c4ef9b
Alex Roman Mamani <aroman@chilesystems.com>
2022-05-27 14:38:39 -0400

    Update Fibonacci

--- POE/global1Fibonnaci/main.js
+++ POE/global1Fibonnaci/main.js
@@ -6,31 +6,17 @@ function sucecionFibonacci(n) {
   c = 0;
   for (let i = 0; i < n; i++) {
     console.log(c);
+
     c = a + b;
     a = b;
     b = c;
   }
+
+  return a;
 }
 
-//SOLICITA MEDIANTE PROMPT VARIABLE DE ENTRADA
-let limite = parseInt(
-  prompt(
-    "Sucecion Fibonacci\nIngrese la cantidad de terminos que desea imprimir:",
-    "10"
-  )
-);
+function funcion() {
+  let limite = document.getElementById("numero").value;
 
-if (limite < 1 || Number.isNaN(limite)) {
-  //CONDICIONAL, SI LA VARIABLE DE ENTRADA ES UN NUMERO MENOR QUE 1 O NO ES UN NUMERO.
-  alert(
-    "ERROR\n- El valor ingresado:\n-- Debe ser un numero mayor a 0.\n-- No puede ser una letra o simbolo."
-  );
-} else if (limite == 1) {
-  //CONDICIONAL, SI LA VARIABLE DE ENTRADA ES UNO.
-  console.log("Primer termino de la Suceción Fibonacci:");
-  sucecionFibonacci(limite);
-} else {
-  //CONDICIONAL, SI LA VARIABLE DE ENTRADA ES MAYOR QUE 0
-  console.log("Los primeros " + limite + " terminos de la suceción Fibonacci:");
-  sucecionFibonacci(limite);
+  document.getElementById("resultado").innerHTML = sucecionFibonacci(limite);
 }
