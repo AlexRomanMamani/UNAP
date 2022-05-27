ea4f0074e9d446d2e15adad276d3009d9d11f851
Alex Mamani <97176969+AlexRomanMamani@users.noreply.github.com>
2022-05-26 23:27:38 -0400

    Add files via upload
    Clase de POE Jueves 26/05/22

---
+++ Clase 26-05-22/main.js
@@ -0,0 +1,8 @@
+function mensaje() {
+  //abreviacion de alert, solo en navegadores modernos
+  alert("Hola Mundo!");
+  //comnado completo, aceptado en todos los casos
+  window.alert("Hola Mundo2!");
+}
+
+//mensaje();
