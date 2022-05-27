e7d1d4efe6efe6d223e50e270fa1322f459e6b35
Alex Roman Mamani <aroman@chilesystems.com>
2022-05-27 12:05:48 -0400

    Rename

--- Clase 26-05-22/main.js
+++
@@ -1,8 +0,0 @@
-function mensaje() {
-  //abreviacion de alert, solo en navegadores modernos
-  alert("Hola Mundo!");
-  //comnado completo, aceptado en todos los casos
-  window.alert("Hola Mundo2!");
-}
-
-//mensaje();
