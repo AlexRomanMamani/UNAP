64c5de6824ae0cfbc897a593ac3da9fe8bcff5a5
Alex Roman Mamani <aroman@chilesystems.com>
2022-06-09 19:43:18 -0400

    Practica

--- POE/06-03-22/main.js
+++ POE/06-03-22/main.js
@@ -1,4 +1,3 @@
-/*
 //CREACION DEL NODO
 let newNodo = document.createElement("p");
 
@@ -8,6 +7,11 @@ newNodo.textContent = "Este es un parrafo generado";
 //CLONAR NODO
 let newNodo2 = newNodo.cloneNode(true);
 let newNodo3 = newNodo.cloneNode(true);
+let newNodo4 = newNodo3.cloneNode(true);
+
+newNodo2.textContent = "Hola este es un parrafo newNodo2";
+newNodo3.textContent = "Este es el parrafo newNodo3";
+newNodo4.textContent = "Este es el newNodo4";
 
 //PUNTO DE INCERSION (NODO)
 let ptoIncersion = document.getElementById("cuerpo");
@@ -16,12 +20,15 @@ let ptoIncersion = document.getElementById("cuerpo");
 ptoIncersion.appendChild(newNodo);
 //INSERTA AL INICIO
 ptoIncersion.prepend(newNodo2);
+ptoIncersion.prepend(newNodo4);
 
 //NUEVO PUNTO DE INSERCION
 let hermano = document.getElementById("p3");
+let hermano2 = document.getElementById("p4");
 
 //INSERTA DONDE SE LE INDICA. INSERTAR ANTES DE (NODO QUE SE INSERTA, SIBLING)
-ptoIncersion.insertBefore(newNodo3, hermano)
+ptoIncersion.insertBefore(newNodo3, hermano);
+ptoIncersion.insertBefore(newNodo4, hermano2);
 
 //ELIMINAR NODO
 //NUEVO PUNTO DE INSERCION
@@ -30,23 +37,29 @@ let nodoEliminar = document.getElementById("p5");
 //hijo.lePideAlPadre.remuevaHijo(el mismo se pide eliminar)
 nodoEliminar.parentNode.removeChild(nodoEliminar);
 
-*/
+//NUEVO PUNTO DE INSERCION PARA EDITAR P6
+let editarP = document.getElementById("p6");
+
+editarP.textContent = "Hola aqui esta el Parrafo 6 Papuuuuss!";
+
 //AGREGAR NODOS A BOTONES
 function generarParrafos() {
-    //CREAR NODO
-    let newNodo = document.createElement("p");
-    //DAR VALOR AL NODO
-    newNodo.textContent = "Este parrafo se genera con la funcion.";
-    //CREAR PUNTO INSERCION
-    let ptoInsercion = document.getElementById("cuerpo");
-    ptoInsercion.appendChild(newNodo);
+  //CREAR NODO
+  let newNodo = document.createElement("p");
+  //DAR VALOR AL NODO
+  newNodo.textContent = "Este parrafo se genera con la funcion.";
+  //CREAR PUNTO INSERCION
+  let ptoInsercion = document.getElementById("cuerpo");
+  ptoInsercion.appendChild(newNodo);
 }
 
 function eliminarParrafos() {
-    //ELIMINAR NODO
-    //NUEVO PUNTO DE INSERCION
-    let ptoInsercion = document.getElementById("cuerpo");
+  //ELIMINAR NODO
+  //NUEVO PUNTO DE INSERCION
+  let ptoInsercion2 = document.getElementById("cuerpo");
 
-    //hijo.lePideAlPadre.remuevaHijo(el mismo se pide eliminar)
-    ptoInsercion.parentNode.removeChild(newNodo);
+  alert(ptoInsercion2);
+
+  //hijo.lePideAlPadre.remuevaHijo(el mismo se pide eliminar)
+  ptoInsercion2.parentNode.removeChild(ptoInsercion2);
 }
