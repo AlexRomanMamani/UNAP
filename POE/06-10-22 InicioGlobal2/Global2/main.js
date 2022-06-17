7544e44a1e5a50de7d15900f40cea635122ce3f2
Alex Roman Mamani <aroman@chilesystems.com>
2022-06-17 03:18:27 -0400

    Update

--- POE/06-10-22 InicioGlobal2/Global2/main.js
+++ POE/06-10-22 InicioGlobal2/Global2/main.js
@@ -1,4 +1,56 @@
 function agregarRegistro() {
+  //------------SACAR PRIMERA LETRA DE APELLIDO------------
+  let apellido = document.getElementById("apellido");
+  let primeraLetra = apellido.value[0];
+
+  const abc = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "ñ",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  for (let i = 0; i < abc.length; i++) {
+    if (primeraLetra === abc[i]) {
+      // CREAR PTOINSERCION TABLE
+      /*let A = document.getElementById("tableA");
+    
+    // CREAR ELEMENTO <tr> y <td>
+    let fila = document.createElement("tr");
+    let celda = document.createElement("td");
+
+    // CREAR CELDAS
+    let textoCelda = document.createTextNode("Hola Mundo!");
+    celda.appendChild(textoCelda);
+    fila.appendChild(celda);
+    A.appendChild(fila);*/
+    } else {
+    }
+  }
+
   let arrayRegistro = [];
 
   arrayRegistro[0] = "";
@@ -9,7 +61,7 @@ function agregarRegistro() {
   arrayRegistro[5] = document.getElementById("telefono");
   arrayRegistro[6] = document.getElementById("organizacion");
 
-  alert(arrayRegistro.length);
+  //alert(arrayRegistro.length);
 
   //for (let i = 0; i < arrayRegistro.length; i++) {
   //alert(arrayRegistro[i].value);
@@ -19,17 +71,4 @@ function agregarRegistro() {
   //var cadena = "Hola mundo";
   //var primerCaracter = cadena.charAt(0);
   // primerCaracter es H
-
-  // CREAR PTOINSERCION TABLE
-  let A = document.getElementById("tableA");
-
-  // CREAR ELEMENTO <tr> y <td>
-  let fila = document.createElement("tr");
-  let celda = document.createElement("td");
-
-  // CREAR CELDAS
-  let textoCelda = document.createTextNode("Hola Mundo!");
-  celda.appendChild(textoCelda);
-  fila.appendChild(celda);
-  A.appendChild(fila);
 }
